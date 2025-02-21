17f3f7d95680bb43287d32fc93c496bf5fcefe62
Izunnaya <ugorjimicheal.um@gmail.com>
2025-02-21 19:11:40 +0100

    Next config file updated to prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "files.edgestore.dev"
-      }
-    ]
-  }
+        hostname: "files.edgestore.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
